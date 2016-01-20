58abe49a39b310a3103595ae8b9a8ca8d54f162d
Justinzenki <jgarcia02@sfo.yearup.org>
2016-01-20 10:15:23 -0800

    edited script

--- Automation.py
+++ Automation.py
@@ -1,11 +1,16 @@
 from selenium import webdriver
 
+print('Input your user name')
+userEmail = input()
+print('Input your password')
+
 browser = webdriver.Firefox()
 browser.get('http://https://login.microsoftonline.com/login.srf?wa=wsignin1.0&rpsnv=4&ct=1453277609&rver=6.7.6640.0&wp=MCMBI&wreply=https:%2F%2Fportal.office.com%2Flanding.aspx%3Ftarget%3D%252fdefault.aspx&lc=1033&id=501392&msafed=0')
 
-emailElem = ('type your email')
 emailElem = browser.find_element_by_id('cred_userid_inputtext')
+emailElem.send_keys(userEmail)
 
-passwordElem = ('type your password')
 passwordElem = browser.find_element_by_id('cred_userid_inputtext')
+passwordElem.send_keys(userPassword)
 passwordElem.submit()
+
